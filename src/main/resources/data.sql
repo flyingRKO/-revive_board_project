ea606db3b0e6340bbc8937403a6fa9b03ee0aa3b
Lee Jae Hun <wogns970@naver.com>
2023-02-02 13:31:19 +0900

    [이재훈] 테스트 데이터 추가
    - 지금까지 작성한 코드를 테스트하고자 테스트 데이터를 추가

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,3 +5,13 @@ values ('admin01', '$2a$10$LxQGnpIA2VLANZNTfSwnwuuVyzvIzEmT0.3OrH.NM15khYIUYxX..
 /*아이디:chan0729 비밀번호: chan0729*/
 insert into member (member_id, member_password, name, phone, term, role)
 values ('chan0729', '$2a$10$QNgQVbxlHVim/zIbkoPcOe7EYvHB2BfHEufLHYwnzUCZgPMuuG/4W', '찬', '01012345678', 1, 'USER');
+
+-- 게시글
+    insert into board (board_type, title, content, registered_date, modified_date) values
+    ('FORUM','test title','test test test','2023-01-01 09:14:25', '2023-01-01 09:20:25'),
+    ('FORUM','test2 title','test2 test2 test2', '2023-01-02 09:14:25','2023-01-02 09:24:25');
+
+-- 댓글
+    insert into board_comment (board_id, content, registered_date) values
+    (1,'lololol','2023-01-01 10:14:25'),
+    (2,'wowowow','2023-01-02 10:14:25')
