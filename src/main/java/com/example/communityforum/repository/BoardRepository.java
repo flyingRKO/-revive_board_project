8c715e208792dcbec8162ee06d7cb2a644c73afd
Lee Jae Hun <wogns970@naver.com>
2023-02-02 13:30:26 +0900

    [이재훈] repository interface 작성
    - Spring Data JPA의 기능을 사용하여 DB에 접근하고자 도메인마다 repository interface 작성

---
+++ src/main/java/com/example/communityforum/repository/BoardRepository.java
@@ -0,0 +1,7 @@
+package com.example.communityforum.repository;
+
+import com.example.communityforum.domain.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+}
