a79eee8944eee5e903bc7a77ef8c925ab9568208
Lee Jae Hun <wogns970@naver.com>
2023-02-04 01:09:38 +0900

    [이재훈] Spring Data REST, Querydsl을 reposity에 적용
    - Spring Data REST를 사용하기 위해 @RepositoryRestResource 추가
    - QuerydslPredicateExecutor, QuerydslBinderCustomizer를 사용하여 api 검색 옵션 추가

--- src/main/java/com/example/communityforum/repository/BoardCommentRepository.java
+++ src/main/java/com/example/communityforum/repository/BoardCommentRepository.java
@@ -1,7 +1,26 @@
 package com.example.communityforum.repository;
 
 import com.example.communityforum.domain.BoardComment;
+import com.example.communityforum.domain.QBoardComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface BoardCommentRepository extends JpaRepository<BoardComment, Long> {
+@RepositoryRestResource
+public interface BoardCommentRepository extends
+        JpaRepository<BoardComment, Long>,
+        QuerydslPredicateExecutor<BoardComment>,
+        QuerydslBinderCustomizer<QBoardComment>
+{
+    @Override
+    default void customize(QuerydslBindings bindings, QBoardComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.content, root.registeredDate); // 추후 user 정보도 필요
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.registeredDate).first(DateTimeExpression::eq);
+    }
 }
