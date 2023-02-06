757c1025ad142f85764b0c742c54d9c89b8dd0e5
chan <seulchan07@gmail.com>
2023-02-06 15:44:11 +0900

    [황슬찬] spring security 설정 및 서비스 추가
    로그인 , 세션유지 및 해지에 필요한 스프링 시큐리티 설정 및 서비스 추가

---
+++ src/main/java/com/example/communityforum/service/MemberSecurityService.java
@@ -0,0 +1,45 @@
+package com.example.communityforum.service;
+
+import com.example.communityforum.domain.member.Member;
+import com.example.communityforum.domain.member.MemberRole;
+import com.example.communityforum.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+@Service
+public class MemberSecurityService implements UserDetailsService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String memberId) throws UsernameNotFoundException {
+        Optional<Member> _siteUser = this.memberRepository.findByMemberId(memberId);
+        if (_siteUser.isEmpty()) {
+            throw new UsernameNotFoundException("사용자를 찾을수 없습니다.");
+        }
+
+        Member siteUser = _siteUser.get();
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        MemberRole _siteUserRole = siteUser.getRole();
+
+        if (MemberRole.ADMIN.getValue().equals(_siteUserRole)) {
+            authorities.add(new SimpleGrantedAuthority(MemberRole.ADMIN.getValue()));
+        } else {
+            authorities.add(new SimpleGrantedAuthority(MemberRole.USER.getValue()));
+        }
+        return new User(siteUser.getMemberId(), siteUser.getMemberPassword(), authorities);
+    }
+}
