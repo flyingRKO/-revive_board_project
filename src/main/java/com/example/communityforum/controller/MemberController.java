b2347ffc6db0bac3d8ce44b7ba0c81604bb37e94
chan <seulchan07@gmail.com>
2023-02-06 19:31:28 +0900

    [황슬찬] 회원가입 컨트롤러 및 서비스 추가

--- src/main/java/com/example/communityforum/controller/MemberController.java
+++ src/main/java/com/example/communityforum/controller/MemberController.java
@@ -1,20 +1,65 @@
 package com.example.communityforum.controller;
 
+import com.example.communityforum.dto.MemberCreateForm;
 import com.example.communityforum.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
+
 
 @RequiredArgsConstructor
 @RequestMapping("/member")
 @Controller
 public class MemberController {
 
-    private final MemberService service;
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public String login() {
         return "/member/login_form";
+    }
+
+    @GetMapping("/signup")
+    public String signup(MemberCreateForm memberCreateForm) {
+        return "/member/signup_form";
+    }
+
+    @PostMapping("/signup")
+    public String signup(@Valid MemberCreateForm memberCreateForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "/member/signup_form";
+        }
+
+        if (!memberCreateForm.getMemberPassword().equals(memberCreateForm.getMemberPassword2())) {
+            bindingResult.rejectValue("memberPassword2", "passwordInCorrect",
+                    "2개의 패스워드가 일치하지 않습니다.");
+            return "/member/signup_form";
+        }
+
+        try {
+            memberService.create(
+                    memberCreateForm.getMemberId(),
+                    memberCreateForm.getMemberPassword(),
+                    memberCreateForm.getName(),
+                    memberCreateForm.getPhone(),
+                    memberCreateForm.getTerm()
+            );
+        } catch (DataIntegrityViolationException e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", "이미 등록된 사용자 ID 입니다.");
+            return "/member/signup_form";
+        } catch (Exception e) {
+            e.printStackTrace();
+            bindingResult.reject("signupFailed", e.getMessage());
+            return "/member/signup_form";
+        }
+
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/communityforum/service/MemberService.java
+++ src/main/java/com/example/communityforum/service/MemberService.java
@@ -3,6 +3,9 @@ package com.example.communityforum.service;
 import com.example.communityforum.domain.member.Member;
 import com.example.communityforum.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,12 +15,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+    //test용 코드
+//    public void saveMember(Member member) {
+//        memberRepository.save(member);
+//    }
+//
+//    public Member getMember(String memberId) {
+//        return memberRepository.findByMemberId(memberId).orElseThrow();
+//    }
 
-    public void saveMember(Member member) {
+    public Member create(String memberId, String memberPassword, String name, String phone, Boolean term) {
+        Member findMember = memberRepository.findByMemberId(memberId).orElse(null);
+        if (findMember != null) {
+            throw new DataIntegrityViolationException("이미 존재하는 아이디입니다.");
+        }
+        Member member = Member.of(memberId,  passwordEncoder.encode(memberPassword), name, phone, term);
         memberRepository.save(member);
-    }
-
-    public Member getMember(String memberId) {
-        return memberRepository.findByMemberId(memberId).orElseThrow();
+        return member;
     }
 }
