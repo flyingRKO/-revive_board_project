d57a1c2b74f55dff75711000f466253bb5e869d5
chan <seulchan07@gmail.com>
2023-02-06 23:46:20 +0900

    [황슬찬] 멤버 서비스 테스트 함수 제거

--- src/test/java/com/example/communityforum/service/MemberServiceTest.java
+++ src/test/java/com/example/communityforum/service/MemberServiceTest.java
@@ -2,6 +2,7 @@ package com.example.communityforum.service;
 
 import com.example.communityforum.domain.member.Member;
 import com.example.communityforum.domain.member.MemberRole;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,12 +15,4 @@ class MemberServiceTest {
     @Autowired
     private MemberService memberService;
 
-    @Test
-    void save_user() {
-        Member member = new Member("test", "test", "test", "132", MemberRole.ADMIN, true);
-//        memberService.saveMember(member);
-//
-//        Member savedUser = memberService.getMember("test");
-        //assertEquals(savedUser.getMemberId(), member.getMemberId());
-    }
 }
