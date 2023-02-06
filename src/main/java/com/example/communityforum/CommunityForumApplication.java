9a2de7f0064319240d04735ada8fa5c042e3efde
Lee Jae Hun <wogns970@naver.com>
2023-02-06 21:59:14 +0900

    [이재훈] 타임리프 Decoupled Logic
    - 타임리프의 Decoupled Logic 설정

--- src/main/java/com/example/communityforum/CommunityForumApplication.java
+++ src/main/java/com/example/communityforum/CommunityForumApplication.java
@@ -2,6 +2,7 @@ package com.example.communityforum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @ConfigurationPropertiesScan
