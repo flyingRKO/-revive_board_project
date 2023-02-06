e001c68f234e9d49c1992c092c84554c55aebf53
YeonHee <lyh951212@naver.com>
2023-02-06 23:58:24 +0900

    Revert "Revert "[이연희] /faq 페이지에서 type 별로 출력되도록 하기""
    This reverts commit 413538b7016384438d3d413f5d55da163e5f329f.

--- src/main/java/com/example/communityforum/controller/FaqController.java
+++ src/main/java/com/example/communityforum/controller/FaqController.java
@@ -6,21 +6,25 @@ import com.example.communityforum.dto.FaqRegisterForm;
 import com.example.communityforum.service.FaqService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.time.LocalDateTime;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
