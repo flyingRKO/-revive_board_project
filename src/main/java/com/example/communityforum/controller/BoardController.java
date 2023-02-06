59dc84aac3b0e8fa66050ee0e2a3a67f85bbbce5
Lee Jae Hun <wogns970@naver.com>
2023-02-06 22:05:18 +0900

    [이재훈] 뷰 기본 골격 잡기
    - 공통 홈, 헤더, 푸터 추가
    - 게시판, 게시글 기본 골격 추가
    - 이에 맞는 컨트롤러 추가

--- src/main/java/com/example/communityforum/controller/BoardController.java
+++ src/main/java/com/example/communityforum/controller/BoardController.java
@@ -1,13 +1,31 @@
 package com.example.communityforum.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
+@RequestMapping("/boards")
 @Controller
 public class BoardController {
 
-    @GetMapping("/home")
-    String home() {
-        return "home";
+    @GetMapping()
+    public String boards(ModelMap map) {
+        map.addAttribute("boards", List.of());
+        return "boards/index";
     }
+
+    @GetMapping("/{boardId}")
+    public String board(@PathVariable Long boardId, ModelMap map){
+        map.addAttribute("board", "board");
+        map.addAttribute("boardComments", List.of());
+
+        return "boards/detail";
+
+    }
+
+
 }
